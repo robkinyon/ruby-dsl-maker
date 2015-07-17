40ea3cacf819565a72d2d7208a12c588ee873416
Rob Kinyon <rob.kinyon@gmail.com>
2015-07-17 16:15:44 -0400

    Refactor the spec_helper to have the SimpleCov section outside the RSpec section

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,15 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 require 'simplecov'
 
-RSpec.configure do |config|
-  SimpleCov.start do
-    minimum_coverage 100
-    refuse_coverage_drop
-  end
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage 100
+  refuse_coverage_drop
 end
 
 require 'dsl/maker'
