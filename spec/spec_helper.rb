529e6cf1e8d6f49b51be056c83f1bab1297a0330
Rob Kinyon <rob.kinyon@gmail.com>
2015-07-21 15:21:58 -0400

    Added linkage from Travis to Codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,31 @@
+require File.expand_path('on_what', File.dirname(File.dirname(__FILE__)))
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 end
 
-begin
-  require 'simplecov'
+unless on_1_8?
+  begin
+    require 'simplecov'
 
-  SimpleCov.configure do
-    add_filter '/spec/'
-    minimum_coverage 100
-    refuse_coverage_drop
+    SimpleCov.configure do
+      add_filter '/spec/'
+      add_filter '/vendor/'
+      minimum_coverage 100
+      refuse_coverage_drop
+    end
+
+    if on_travis?
+      require 'codecov'
+      SimpleCov.formatter = SimpleCov::Formatter::Codecov
+    end
+
+    SimpleCov.start
+  rescue LoadError
+    puts "Coverage is disabled - install simplecov to enable."
   end
-
-  SimpleCov.start
-rescue LoadError
-  puts "Coverage is disabled - install simplecov to enable."
 end
 
 require 'dsl/maker'
